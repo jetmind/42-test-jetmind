0aa94f76d2c8b5e62ad4d1805e08af335b642f98
Igor Bondarenko <jetmind2@gmail.com>
2011-06-15 18:49:10 +0300

    Revert "ticket:6 Added jQuery datapicker to birth form field"
    This reverts commit bf3dfc5b4093f3a66138c832fe4614c6ceaee540.

--- testjet/urls.py
+++ testjet/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from testjet import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,7 +18,5 @@ urlpatterns = patterns('',
     (r'^requests/', include('testjet.requestmiddle.urls')),
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, 'login'),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, 'logout'),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-        {'document_root': settings.STATIC_DOC_ROOT}, 'static'),
     (r'', include('testjet.contact.urls')),
 )
