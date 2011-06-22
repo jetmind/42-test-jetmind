acefef839c16e84835c37c499a5e2742506e1e0f
Igor Bondarenko <jetmind2@gmail.com>
2011-06-22 20:05:08 +0300

    ticket:13 Added priority field to StoredHttpRequest

--- testjet/requestmiddle/models.py
+++ testjet/requestmiddle/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class StoredHttpRequest(models.Model):
     path = models.CharField(max_length=255)
     method = models.CharField(max_length=4)
     created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField(default=1)
 
     def __unicode__(self):
         return "%s %s at %s" % (self.method, self.path, str(self.created_at))
