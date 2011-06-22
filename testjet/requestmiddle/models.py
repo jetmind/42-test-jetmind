6e6f7eca69e9feb55d12a59fd6e3becb37c57a5f
Igor Bondarenko <jetmind2@gmail.com>
2011-06-22 21:03:43 +0300

    ticket:13 Now user can set priority

--- testjet/requestmiddle/models.py
+++ testjet/requestmiddle/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class StoredHttpRequest(models.Model):
+    PRIORITIES = ((i, i) for i in range(1, 11))
     path = models.CharField(max_length=255)
     method = models.CharField(max_length=4)
     created_at = models.DateTimeField(auto_now_add=True)
-    priority = models.IntegerField(default=1)
+    priority = models.IntegerField(default=1, choices=PRIORITIES)
 
     def __unicode__(self):
-        return "%s %s at %s" % (self.method, self.path, str(self.created_at))
+        return "%s %s at %s (priority: %d)" % (self.method, self.path, str(self.created_at), self.priority)
