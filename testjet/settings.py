54f58b9ac7de1c0e7df16d7ba65dffc7bf52fb21
Igor Bondarenko <jetmind2@gmail.com>
2011-07-06 13:01:46 +0300

    ticket:14 Added django widget for calendar

--- testjet/settings.py
+++ testjet/settings.py
@@ -53,7 +53,9 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+STATIC_DOC_ROOT = os.path.join(CURRENT_PATH, 'static')
+
+MEDIA_URL = '/static/'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/edit/'
@@ -62,8 +64,6 @@ LOGIN_REDIRECT_URL = '/edit/'
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
-
-STATIC_DOC_ROOT = os.path.join(CURRENT_PATH, 'static')
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'z$0je5_-g118tfigx76&t+*)kf9io^fewk=awgh_wo(3s%by6o'
